644e33e1e1a20103844be62c23ccbf535c26fe33
Abdurrahman Alowain <47498835+A-Owain@users.noreply.github.com>
2025-03-24 03:34:31 +0300

    eid_greeting_app.py

--- eid_greeting_app.py
+++ eid_greeting_app.py
@@ -10,7 +10,7 @@ IMAGE_PATH = "eid-fitr.jpg"
 FONT_PATH = "NotoSansArabic-SemiBold.ttf"  # Ensure this font file is in the repo root
 
 st.set_page_config(page_title="Eid Greeting Generator", layout="centered")
-st.title("🎉 Eid Greeting Generator")
+st.title("TRAY Eid Greeting Generator")
 
 name = st.text_input("Enter Your Name | ادخل اسمك", max_chars=30)
 position = st.text_input("Position (Optional) | المسمى الوظيفي (اختياري)", max_chars=30)
