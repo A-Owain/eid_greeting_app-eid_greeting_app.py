2576c6570e585db204bf5467c87618e3b9ce0fc6
Abdurrahman Alowain <47498835+A-Owain@users.noreply.github.com>
2025-03-26 18:09:22 +0300

    Update eid_greeting_app.py

--- eid_greeting_app.py
+++ eid_greeting_app.py
@@ -50,7 +50,7 @@ if name:
         pos_bbox = position_font.getbbox(bidi_pos)
         pos_width = pos_bbox[2] - pos_bbox[0]
         x_pos = (image_width - pos_width) / 2
-        y_pos = y_name + 110
+        y_pos = y_name + 120
         draw.text((x_pos, y_pos), bidi_pos, font=position_font, fill="#EA2F2F")  # red
 
     # Convert to bytes
